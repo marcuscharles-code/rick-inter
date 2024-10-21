d5e2f682e90811c35fb38f0d07cfa8e19c4dafc1
Richard-Raph <richardakpan77@gmail.com>
2024-10-21 18:37:25 +0100

    "Added Service route and component, updated Hero navigation, and removed unnecessary code from Home component"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Home from './routes/Home';
+import Service from './routes/Service';
 import Layout from './components/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/service' element={<Service />} />
         </Routes>
       </Layout>
     </Router>
---
+++ client/src/routes/Service.jsx
@@ -0,0 +1,9 @@
+import Hero from '../pages/Hero';
+
+export default function Service() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
