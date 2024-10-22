e6a883baef2b5158202ee00fc555f70de894de17
marcus charles <charlesfatunsin@gmail.com>
2024-10-23 00:20:42 +0100

    "Removed App.css content, updated App.jsx to import Bootstrap CSS and render Home component, and removed React and Vite logos and associated code."

---
+++ client/src/components/NavInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavInfo() {
+  return (
+    <div>
+        <div><h1><span></span>2231 Johnstown Road Bensenville, IL 60106</h1></div>
+    </div>
+  )
+}
+
+export default NavInfo
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import NavbarTop from '../components/Navbar'
+
+function Home() {
+  return (
+    <div>
+        <NavbarTop />
+    </div>
+  )
+}
+
+export default Home;
