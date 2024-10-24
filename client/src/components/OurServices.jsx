c29dcf5ddcce4a65f5dc36ece3442904c84cd07d
marcus charles <charlesfatunsin@gmail.com>
2024-10-24 05:26:24 +0100

    "Updated About.css and About.jsx files with changes to styling and layout of About section, including addition of new CSS classes and React components."

---
+++ client/src/components/OurServices.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/css/Services.css'
+
+function OurServices() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default OurServices
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,29 +1,57 @@
-import ceo from '../assets/images/ceo.webp';
-import bg from '../assets/images/bg.webp';
-import '../assets/css/About.css';
+import ceo from "../assets/images/ceo.webp";
+import bg from "../assets/images/bg.webp";
+import "../assets/css/About.css";
+import { FaGreaterThan } from "react-icons/fa";
 
 function About() {
   return (
-    <section className='about-section'>
-      <div className='abt-img'>
-        <img className='img-top' src={bg} alt="Background" />
-        <img className='img-bottom' src={ceo} alt="CEO" />
+    <section className="about-section">
+      <div className="abt-img">
+        <img className="img-top" src={bg} alt="Background" />
+        <img className="img-bottom" src={ceo} alt="CEO" />
       </div>
-      <div className='abt-content'>
-        <div>
-          <h1>Preye Allison </h1>
+      <div className="abt-content">
+        <div className="founder-name">
+          <span>
+            {" "}
+            Preye <mark>Allison</mark>{" "}
+          </span>
           <span>Founder</span>
         </div>
-        <div>
-          <p>Since 1999, Weldo has been servicing all of San Diego. Our mission is to work diligently and efficiently with every client ensuring a smooth and painless process, while we make their vision into reality.</p>
-          <p>With our knowledge and experience, it is our goal to attend to our clients needs, going above and beyond their expectations. Longs Welding is your choice for the highest quality and most innovative ideas in iron metal works and ornamentals.</p>
+        <div className="about-txt">
+          <p>
+            Since 1999, Weldo has been servicing all of San Diego. Our mission
+            is to work diligently and efficiently with every client ensuring a
+            smooth and painless process, while we make their vision into
+            reality.
+          </p>
+
+          <hr></hr>
+
+          <p>
+            With our knowledge and experience, it is our goal to attend to our
+            clients needs, going above and beyond their expectations. Longs
+            Welding is your choice for the highest quality and most innovative
+            ideas in iron metal works and ornamentals.
+          </p>
         </div>
-        <div>
-          <h1>Why Choose Us</h1>
-          <div>
-            <p><span></span>Superior precise quality welds</p>
-            <p><span></span>Welds can be made with little or no filler</p>
-            <p><span></span>Precise control of welding variables</p>
+        <div className="why-us">
+          <h1>
+            Why <mark>Choose Us</mark>
+          </h1>
+          <div className="reasons">
+            <div>
+              <mark><FaGreaterThan /></mark> 
+              <p>Superior precise quality welds</p>
+            </div>
+            <div>
+              <mark><FaGreaterThan /></mark> 
+              <p>Welds can be made with little or no filler</p>
+            </div>
+            <div>
+              <mark><FaGreaterThan />
+              </mark> <p>Precise control of welding variables</p>
+            </div>            
           </div>
         </div>
       </div>
