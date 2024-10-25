c8ab3e995347afd7eda37131656658326d486109
marcus charles <charlesfatunsin@gmail.com>
2024-10-25 18:00:35 +0100

    "Update App.css, App.jsx, Footer.css, Hero.css, and Top.jsx with various styling and layout changes"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import './App.css'
+import './App.css';
 import Home from './routes/Home';
 import About from './routes/About';
 import Service from './routes/Service';
 import Contact from './routes/Contact';
 import Layout from './components/Layout';
 import Portfolio from './routes/Portfolio';
+import { LoadTop, PageTop } from './components/Top';
 import { TitleProvider } from './helpers/TitleContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <TitleProvider>
       <Router>
+        <LoadTop />
         <Layout>
           <Routes>
             <Route path='/' element={<Home />} />
@@ -22,9 +23,10 @@ function App() {
             <Route path='/portfolio' element={<Portfolio />} />
           </Routes>
         </Layout>
+        <PageTop />
       </Router>
     </TitleProvider>
-  )
+  );
 }
 
 export default App;
