9f6fc69e850116e51e9e285d3b1be460a9d0e762
marcus charles <charlesfatunsin@gmail.com>
2024-10-31 12:53:30 +0100

    "Rename 'Waste' import to 'waste' in Services.jsx"

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import wellheadIcon from '../assets/images/wellheadIcon.png';
 import PipeInspection from '../assets/images/PipeInspection.png';
 import maintainanceIcon from '../assets/images/maintainanceIcon.png';
-import Waste from '../assets/images/Waste.png';
+import waste from '../assets/images/waste.png';
 import globalP from '../assets/images/globalP.png';
 import manPower from '../assets/images/manPower.png';
 
@@ -12,7 +12,7 @@ export default function Services() {
     { id: 1, icon: wellheadIcon, url: '/', title: 'Wellhead', subtitle: 'Installation', description: 'There are a few different processes that we use at JA' },
     { id: 2, icon: PipeInspection, url: '/', title: 'Pipe', subtitle: 'Inspection', description: 'We offer precise steel cutting services for various needs' },
     { id: 3, icon: maintainanceIcon, url: '/', title: 'Equipment', subtitle: 'Maintenance', description: 'Professional metal polishing services for a perfect finish' },
-    { id: 4, icon: Waste, url: '/', title: 'Waste', subtitle: 'Management', description: 'Expert welding services for aluminum projects' },
+    { id: 4, icon: waste, url: '/', title: 'Waste', subtitle: 'Management', description: 'Expert welding services for aluminum projects' },
     { id: 5, icon: globalP, url: '/', title: 'Global', subtitle: 'Procurement', description: 'Tailored metal fabrication solutions to meet any requirement' },
     { id: 6, icon: manPower, url: '/', title: 'Manpower', subtitle: 'Supply', description: 'Advanced laser cutting services for precise results' }
   ];
